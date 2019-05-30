3541827ce3dd2ed71cb009dbc8323a1db23638ac
hollyrc1815 <hollyrc1815@gmail.com>
2019-05-30 16:28:25 -0500

    blogPost

---
+++ src/main/java/com/techtalentsouth/TechTalentBlog/BlogPostRepository.java
@@ -0,0 +1,7 @@
+package com.techtalentsouth.TechTalentBlog;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogPostRepository extends CrudRepository<BlogPost, Long>{
+
+}
